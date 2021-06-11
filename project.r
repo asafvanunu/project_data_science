f50d8e619959015e3e47c2c66f86401a0e9cd69e
asafvanunu <asafyu@post.bgu.ac.il>
2021-06-11 14:58:42 +0300

    ## 2st' - adding script with library and tables!

---
+++ project.r
@@ -0,0 +1,16 @@
+## Load library
+library(tidyverse)
+
+## Read CSV files
+Budget = read.csv("Budget_plus.csv")
+Electricity_consumption = read.csv("Electricity_consumption.csv")
+Electricity_from_fossil_fuels = read.csv("Electricity_from_fossil_fuels.csv")
+Electricity_production = read.csv("Electricity_production.csv")
+EPI_2020 = read.csv("EPI_2020.csv")
+Exports = read.csv("exports.csv")
+Industrial_production_growth = read.csv("Industrial_production_growth_rate.csv")
+Labor_force = read.csv("Labor_force.csv")
+Revenue_from_coal = read.csv("Revenue_from_coal.csv")
+Revenue_from_forest_resources = read.csv("Revenue_from_forest_resources.csv")
+Unemployment_rate = read.csv("Unemployment_rate.csv")
+World_data = read.csv("World_data.csv")
