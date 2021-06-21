338724ec0a167a97e4d316d9fb3feb0454ab863f
asafvanunu <asafyu@post.bgu.ac.il>
2021-06-21 17:37:53 +0300

    more graph

--- energy_con.r
+++ energy_con.r
@@ -24,27 +24,42 @@ EW_model=World%>%
   
 
 
-
+##Middle
 EM_model%>%
   filter(value_kWh<1e+11)%>%
   ggplot(aes(x = value_kWh , y = EPI_new)) +
   labs(title = "EPI Vs Electricity consumption - middle income", x = "kWh", y = "EPI score") +
   geom_point(aes(colour = region)) + geom_smooth(method = "lm", se = FALSE, colour = "red")
 
+## statistics
+summary(lm(EPI_new ~ value_kWh , data = EM_model))
+
+##Low
 EL_model%>%
   ggplot(aes(x = value_kWh , y = EPI_new)) +
   labs(title = "EPI Vs Electricity consumption - Low income", x = "kWh", y = "EPI score") +
   geom_point(aes(colour = Country_Name, size = region)) + geom_smooth(method = "lm", se = FALSE, colour = "red")
 
+## statistics
+summary(lm(EPI_new ~ value_kWh , data = EL_model))
+
+
+##High
 EH_model%>%
   filter(value_kWh<1e+11)%>%
   ggplot(aes(x = value_kWh , y = EPI_new)) +
   labs(title = "EPI Vs Electricity consumption - High income", x = "kWh", y = "EPI score") +
   geom_point(aes(colour = Country_Name, size = region)) + geom_smooth(method = "lm", se = FALSE, colour = "red")
 
+## statistics
+summary(lm(EPI_new ~ value_kWh , data = EH_model))
+
+## World
 EW_model%>%
   filter(value_kWh<1e+11)%>%
   ggplot(aes(x = value_kWh , y = EPI_new)) +
   labs(title = "EPI Vs Electricity consumption - World", x = "kWh", y = "EPI score") +
-  geom_point(aes(size = region)) + geom_smooth(method = "lm", se = FALSE, colour = "red")  
+  geom_point(aes(colour = region)) + geom_smooth(method = "lm", se = FALSE, colour = "red")  
   
+## statistics
+summary(lm(EPI_new ~ value_kWh , data = EW_model))
---
+++ industrial.r
@@ -0,0 +1,65 @@
+##Middle
+IM_model=Middle%>%
+  filter(Time == 2017)%>%
+  inner_join(EPI_2020, by = c("Country_Name"="country"))%>%
+  inner_join(Industrial_production_growth, by = c("Country_Name"="name"))
+
+
+##Low
+IL_model=Low%>%
+  filter(Time == 2017)%>%
+  inner_join(EPI_2020, by = c("Country_Name"="country"))%>%
+  inner_join(Industrial_production_growth, by = c("Country_Name"="name"))
+
+##High
+IH_model=High%>%
+  filter(Time == 2017)%>%
+  inner_join(EPI_2020, by = c("Country_Name"="country"))%>%
+  inner_join(Industrial_production_growth, by = c("Country_Name"="name"))
+
+##World
+IW_model=World%>%
+  filter(Time == 2017)%>%
+  inner_join(EPI_2020, by = c("Country_Name"="country"))%>%
+  inner_join(Industrial_production_growth, by = c("Country_Name"="name"))
+
+
+
+##Middle
+IM_model%>%
+  ggplot(aes(x = value_precent , y = EPI_new)) +
+  labs(title = "EPI Vs Industrial production growth - middle income", x = "%", y = "EPI score") +
+  geom_point(aes(colour = region)) + geom_smooth(method = "lm", se = FALSE, colour = "red")
+
+## statistics
+summary(lm(EPI_new ~ value_precent , data = IM_model))
+
+##Low
+IL_model%>%
+  ggplot(aes(x = value_precent , y = EPI_new)) +
+  labs(title = "EPI Vs Industrial production growth - Low income", x = "%", y = "EPI score") +
+  geom_point(aes(colour = Country_Name, size = region)) + geom_smooth(method = "lm", se = FALSE, colour = "red")
+
+## statistics
+summary(lm(EPI_new ~ value_precent , data = IL_model))
+
+
+##High
+IH_model%>%
+  filter(value_precent<1e+11)%>%
+  ggplot(aes(x = value_precent , y = EPI_new)) +
+  labs(title = "EPI Vs Industrial production growth - High income", x = "%", y = "EPI score") +
+  geom_point(aes(colour = Country_Name, size = region)) + geom_smooth(method = "lm", se = FALSE, colour = "red")
+
+## statistics
+summary(lm(EPI_new ~ value_precent , data = IH_model))
+
+## World
+IW_model%>%
+  filter(value_precent<1e+11)%>%
+  ggplot(aes(x = value_precent , y = EPI_new)) +
+  labs(title = "EPI Vs Industrial production growth - World", x = "%", y = "EPI score") +
+  geom_point(aes(colour = region)) + geom_smooth(method = "lm", se = FALSE, colour = "red")  
+
+## statistics
+summary(lm(EPI_new ~ value_precent , data = IW_model))
